d1056f076e0dfbe0b8aab84fe3eb2406862978a9
dhmizu95 <dhmizu95@gmail.com>
2020-06-24 16:47:22 +0600

    implementation of q5

---
+++ README.md
@@ -0,0 +1,37 @@
+# weDevsQ05
+
+## Que: 5 - to-do list application
+
+You have to create a to-do list application. Please watch the Sample video link below. Your Application has to work exactly like that.
+Sample Video Link: http://bit.ly/2MruXMu
+
+Important Note: You must use Vuex.
+
+### Project setup
+
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+
+```
+npm run database    <!-- to run the json-server -->
+npm run serve       <!-- to run the server -->
+```
+
+### Compiles and minifies for production
+
+```
+npm run build
+```
+
+### Lints and fixes files
+
+```
+npm run lint
+```
+
+### Customize configuration
+
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <h1 id="todo-header">Todos</h1>
+      <AddTodo />
+      <FilterTodos />
+      <Todos />
+    </div>
+  </div>
+</template>
+
+<script>
+import FilterTodos from "./components/FilterTodos.vue";
+import AddTodo from "./components/AddTodo.vue";
+import Todos from "./components/Todos.vue";
+
+export default {
+  name: "App",
+  components: {
+    Todos,
+    AddTodo,
+    FilterTodos
+  }
+};
+</script>
+
+<style>
+@import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
+
+body {
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  line-height: 1.6;
+  background: #e8f7f0;
+}
+.container {
+  max-width: 1100px;
+  margin: auto;
+  overflow: auto;
+  padding: 0 2rem;
+}
+#todo-header {
+  text-align: center;
+}
+</style>
